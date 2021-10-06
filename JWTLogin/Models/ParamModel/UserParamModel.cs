fcd9172b75d552fbbba049ae298a0420288c2083
LAPTOP-04UR9CNS\user <jacky6632003@gmail.com>
2021-10-06 21:56:05 +0800

    add: README

--- JWTLogin/Models/ParamModel/UserParamModel.cs
+++ JWTLogin/Models/ParamModel/UserParamModel.cs
@@ -8,7 +8,7 @@ namespace JWTLogin.Models.ViewModel
 {
     public class UserParamModel
     {
-        [Required(ErrorMessage = "帳號為必填欄位"), MaxLength(32),MinLength(2, ErrorMessage = "帳號長度要大於2")]
+        [Required(ErrorMessage = "帳號為必填欄位"), MaxLength(32),MinLength(3, ErrorMessage = "帳號長度要大於3")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "密碼為必填欄位"), DataType(DataType.Password)]
---
+++ README.md
@@ -0,0 +1,18 @@
+# JWTLogin 範例碼
+
+## 使用方法
+方法一
+1.修改appsettings的db連結
+2 跑SQL/User.sql 新增tabel
+3.run code
+方法二
+run docker-compose up
+
+登入
+![image](./image/1.png)
+註冊畫面
+![image](./image/2.png)
+登入Jwt
+![image](./image/Jwt.png)
+架構
+![image](./image/pattern.jpg)
