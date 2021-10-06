c4e23f9eb22c2f0eee3b8449f9ac8236f202c701
LAPTOP-04UR9CNS\user <jacky6632003@gmail.com>
2021-10-06 16:57:40 +0800

    add : new model

---
+++ JWTLogin.Repository/Model/ConditionModel/UserConditionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JWTLogin.Repository.Model.ConditionModel
+{
+    public class UserConditionModel
+    {
+        public string Username { get; set; }
+    
+        public string Password { get; set; }
+       
+    }
+}
---
+++ JWTLogin/Models/ParamModel/UserParamModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTLogin.Models.ViewModel
+{
+    public class UserParamModel
+    {
+        [Required(ErrorMessage = "帳號為必填欄位"), MaxLength(32),MinLength(2, ErrorMessage = "帳號長度要大於2")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "密碼為必填欄位"), DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(Password), ErrorMessage = "密碼不同")]
+        public string ConfirmPassword { get; set; }
+    }
+}
