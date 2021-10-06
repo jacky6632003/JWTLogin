61d9afdf1b431fffd38187e57973d487b2f15f6b
LAPTOP-04UR9CNS\user <jacky6632003@gmail.com>
2021-10-06 16:56:51 +0800

    add: Aes and contants Config

---
+++ JWTLoginCommon/Config/AesCryptoConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JWTLoginCommon.Config
+{
+    public class AesCryptoConfig
+    {
+        public const string AesCrypto = "AesCrypto";
+
+        public string AesKey { get; set; }
+        public string AesIv { get; set; }
+    }
+}
---
+++ JWTLoginCommon/Config/ConstantsConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JWTLoginCommon.Config
+{
+    public class ConstantsConfig
+    {
+        public const string Constant = "Constant";
+        public string Issuer { get; set; }
+        public string Audiance { get; set; }
+        public string Secret { get; set; }
+    }
+}
