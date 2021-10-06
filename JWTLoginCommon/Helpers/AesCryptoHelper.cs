78edd0615c0363852d672e711caff1efc488a3b0
LAPTOP-04UR9CNS\user <jacky6632003@gmail.com>
2021-10-06 16:57:21 +0800

    add: Aes and Jwt helper

---
+++ JWTLoginCommon/Helpers/AesCryptoHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JWTLoginCommon.Helpers
+{
+   public static class AesCryptoHelper
+    {
+        public static string Encrypt(string key, string iv, string plain_text)
+        {
+
+            Aes aes = Aes.Create();
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform transform = aes.CreateEncryptor(Encoding.UTF8.GetBytes(key), Encoding.UTF8.GetBytes(iv));
+
+            byte[] bPlainText = Encoding.UTF8.GetBytes(plain_text);
+            byte[] outputData = transform.TransformFinalBlock(bPlainText, 0, bPlainText.Length);
+            return Convert.ToBase64String(outputData);
+        }
+
+        public static string Decrypt(string key, string iv, string base64String)
+        {
+            Aes aes = Aes.Create();
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform transform = aes.CreateDecryptor(Encoding.UTF8.GetBytes(key), Encoding.UTF8.GetBytes(iv));
+            byte[] bEnBase64String = null;
+            byte[] outputData = null;
+            try
+            {
+                bEnBase64String = Convert.FromBase64String(base64String);
+                outputData = transform.TransformFinalBlock(bEnBase64String, 0, bEnBase64String.Length);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($@"解密出錯:{ex.Message}");
+            }
+            return Encoding.UTF8.GetString(outputData);
+        }
+    }
+}
---
+++ JWTLoginCommon/Helpers/JwtHelpers.cs
@@ -0,0 +1,48 @@
+﻿using JWTLoginCommon.Config;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTLoginCommon.Helpers
+{
+    public class JwtHelpers
+    {
+        private readonly IConfiguration Configuration;
+
+        public JwtHelpers(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        public string GenerateToken(string userName, ConstantsConfig constantsConfig,int expireHours = 1)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("account", userName)
+            };
+            
+            var secretBytes = Encoding.UTF8.GetBytes(constantsConfig.Secret);
+            var key = new SymmetricSecurityKey(secretBytes);
+            var algorithm = SecurityAlgorithms.HmacSha256;
+
+            var signingCredentials = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                constantsConfig.Issuer,
+                constantsConfig.Audiance,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(expireHours),
+                signingCredentials);
+
+            var tokenJson = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenJson;
+        }
+    }
+}
